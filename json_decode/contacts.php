c2e3d7a03cfa7ff2cb9ece3f8f089e72d16932e8
JoeFamiliar <joefamwork@gmail.com>
2018-10-23 23:56:36 +0300

    Update contacts.php

--- json_decode/contacts.php
+++ json_decode/contacts.php
@@ -11,7 +11,7 @@
 			</tr>
 
 			<?php
-			$content = file_get_contents("./contacts.json");
+			$content = file_get_contents("contacts.json");
 			$contacts = json_decode($content, true);
 			foreach ($contacts as $contact) {
 				echo "<tr><td>$contact['firstName']</td><td>$contact['lastName']</td><td>$contact['address']</td><td>$contact['phoneNumber']</td></tr>";
