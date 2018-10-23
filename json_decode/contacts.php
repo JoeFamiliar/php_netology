b0427c2486d84c40000c8570b9f0d65eacb29274
JoeFamiliar <joefamwork@gmail.com>
2018-10-23 23:54:14 +0300

    json task
    обновил выполнение домашки

--- json_decode/contacts.php
+++ json_decode/contacts.php
@@ -4,17 +4,18 @@
 	<body>
 		<table>
 			<tr>
-				<td></td>
-				<td></td>
-				<td></td>
-				<td></td>
+				<td>Имя</td>
+				<td>Фамилия</td>
+				<td>Адрес</td>
+				<td>Телефон</td>
 			</tr>
 
 			<?php
-			$content = file_get_contents("./contacts.json");
-			$contacts = json_decode($content, true);
+			$json = file_get_contents('contacts.json');
+			$contacts = json_decode($json, true);
+			//print_r($contacts);
 			foreach ($contacts as $contact) {
-				echo "<tr><td>$contact['firstname']</td><td>$contact['lastname']</td><td>$contact['address']</td><td>$contact['phoneNumber']</td></tr>";
+				echo "<tr><td>".$contact['firstName']."</td><td>".$contact['lastName']."</td><td>".$contact['address']."</td><td>".$contact['phoneNumber']."</td></tr>";
 			}
 			?>
 		</table>
