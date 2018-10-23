4121ae8dc0d72ca8672e19008b00424973349978
JoeFamiliar <joefamwork@gmail.com>
2018-10-23 23:55:03 +0300

    Update contacts.php

--- json_decode/contacts.php
+++ json_decode/contacts.php
@@ -14,7 +14,7 @@
 			$content = file_get_contents("./contacts.json");
 			$contacts = json_decode($content, true);
 			foreach ($contacts as $contact) {
-				echo "<tr><td>$contact['firstname']</td><td>$contact['lastname']</td><td>$contact['address']</td><td>$contact['phoneNumber']</td></tr>";
+				echo "<tr><td>$contact['firstName']</td><td>$contact['lastName']</td><td>$contact['address']</td><td>$contact['phoneNumber']</td></tr>";
 			}
 			?>
 		</table>
