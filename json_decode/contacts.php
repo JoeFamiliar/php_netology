eed317d80bdbc0dfaef2f1979167f5df7d8cf5cc
JoeFamiliar <joefamwork@gmail.com>
2018-10-24 04:28:51 +0300

    2
    сделал json сложнее

--- json_decode/contacts.php
+++ json_decode/contacts.php
@@ -6,7 +6,9 @@
 			<tr>
 				<td>Имя</td>
 				<td>Фамилия</td>
+				<td>Город</td>
 				<td>Адрес</td>
+				<td>Почтовый индекс</td>
 				<td>Телефон</td>
 			</tr>
 
@@ -15,7 +17,12 @@
 			$contacts = json_decode($json, true);
 			//print_r($contacts);
 			foreach ($contacts as $contact) {
-				echo "<tr><td>".$contact['firstName']."</td><td>".$contact['lastName']."</td><td>".$contact['address']."</td><td>".$contact['phoneNumber']."</td></tr>";
+				if(is_array($contact['phoneNumber'])){
+					$phone = implode(", ", $contact['phoneNumber']);
+				}
+				else
+					$phone = $contact['phoneNumber'];
+				echo "<tr><td>".$contact['firstName']."</td><td>".$contact['lastName']."</td><td>".$contact['address']['streetAddress']."</td><td>".$contact['address']['city']."</td><td>".$contact['address']['postalCode']."</td><td>".$phone."</td></tr>";
 			}
 			?>
 		</table>
