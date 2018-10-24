a33a7b4cf4f1d3718c76e1e6e165923ca34e2b9d
JoeFamiliar <joefamwork@gmail.com>
2018-10-24 05:09:52 +0300

    2
    2

--- task1_b.php
+++ task1_b.php
@@ -2,18 +2,29 @@
 	Введите проверяемое число: 
 	<input type="text" name="a" value=""><br>
 	Введите крайние числа диапазона: <br>
-	<input type="text" name="left" value="0">
-	<input type="text" name="right" value="100"><br>
 	<input type="submit" value="Проверить">
 </form>
 
 <?php
- $a = $_POST['a'];
- $left = $_POST['left'];
- $right = $_POST['right'];
-
- if($a < $left || $a > $right){
- 	echo "Число выходит за рамки диапазона";
+ $a = (int)$_POST['a'];
+ $fib1 = 1;
+ $fib2 = 1;
+ $check = 1;
+ while($check){
+	if($a < $fib1){
+	 	echo "Число не входит в числовой ряд";
+	 	$check = 0;
+	}
+	else {
+	 	if($a == $fib1){
+	 		echo "Число входит в числовой ряд";
+	 		$check = 0;
+	 	}
+	 	else {
+	 		$fib3 = $fib1;
+	 		$fib1 += $fib2;
+	 		$fib2 = $fib3;
+	 	}
+	}
  }
- else echo "Число входит в рамки";
  ?>
