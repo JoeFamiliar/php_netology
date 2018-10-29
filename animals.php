b4091b1d9bc9b8da5e3b788d8f5c1527c54fb23b
JoeFamiliar <joefamwork@gmail.com>
2018-10-29 23:03:43 +0300

    animals

---
+++ animals.php
@@ -0,0 +1,32 @@
+<?php
+$continents = [
+    'Africa' => [
+        'African elephant',
+        'Hippo',
+        'Giraffe',
+        'Crocodile',
+        'Spotted hyena',
+        'Zebra',
+        'Chimpanzee',
+        'Python'
+    ],
+    'Eurasia' => [
+        'Brown bear',
+        'Wolf',
+        'Fire fox',
+        'Snow leopard',
+        'Varan',
+        'Big panda',
+        'White rabbit'
+    ],
+    'America' => [
+        'Castor canadensis',
+        'Arctic Wolf',
+        'American Elk',
+        'Racoon',
+        'Skunk',
+        'Alligator mississippiensis',
+        'Caribou'
+    ]
+];
+ 
