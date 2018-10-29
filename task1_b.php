4811ea3da1324bf6e7448149d3282b0346d2f2fc
JoeFamiliar <joefamwork@gmail.com>
2018-10-29 21:33:49 +0300

    обновил первое задание

--- task1_b.php
+++ task1_b.php
@@ -1,30 +1,31 @@
-<form method="post">
-	Введите проверяемое число: 
-	<input type="text" name="a" value=""><br>
-	Введите крайние числа диапазона: <br>
-	<input type="submit" value="Проверить">
-</form>
-
 <?php
  $a = (int)$_POST['a'];
  $fib1 = 0;
  $fib2 = 1;
- $check = 1;
- while($check){
+ if(!isset($a)){
+	while($fib1 < $a){
+ 		$fib3 = $fib1;
+ 		$fib1 += $fib2;
+ 		$fib2 = $fib3;
+ 	}
 	if($a < $fib1){
-	 	echo "Число не входит в числовой ряд";
-	 	$check = 0;
+	 	$text = "Число не входит в числовой ряд";
 	}
-	else {
-	 	if($a == $fib1){
-	 		echo "Число входит в числовой ряд";
-	 		$check = 0;
-	 	}
-	 	else {
-	 		$fib3 = $fib1;
-	 		$fib1 += $fib2;
-	 		$fib2 = $fib3;
-	 	}
+	elseif($a == $fib1){
+	 	$text = "Число входит в числовой ряд";
 	}
  }
  ?>
+ <html>
+ 	<head>
+ 	</head>
+ 	<body>
+		 <form method="post">
+			Введите проверяемое число: 
+			<input type="text" name="a" value=""><br>
+			<input type="submit" value="Проверить">
+		</form>
+		Вы ввели: <?php echo $a; ?><br>
+		<?php echo $text; ?>
+	</body>
+</html>
