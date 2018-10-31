5566ebee33aa50af846ebdef2a927670e8cde024
JoeFamiliar <joefamwork@gmail.com>
2018-10-31 22:42:41 +0300

    upd animals

--- animals.php
+++ animals.php
@@ -35,10 +35,12 @@ $two_words_animals = [];
 foreach($continents as $continent => $animals){
     foreach($animals as $animal){
     	if(strpos($animal, " ")){
-    		$two_words_animals[] = $animal;
-    		$parts = explode(' ', $name);
-		    $first[] = $parts[0];
-		    $second[] = $parts[1];
+    		$parts = explode(' ', $animal);
+            if(count($parts) == 2){
+                $two_words_animals[] = $animal;
+                $first[] = $parts[0];
+                $second[] = $parts[1];
+            }
     	}
     }
 }
@@ -48,11 +50,22 @@ shuffle($second);
 $new_animals = [];
 $result = '';
 for($i = 0; $i < count($first); $i++){
-    $new_animals[]= $first[$i] . ' ' . $senond[$i];
-    $result .= $first[$i] . ' ' . $senond[$i].", ";
+    $new_animals[]= $first[$i] . ' ' . $second[$i];
+    $result .= $first[$i] . ' ' . $second[$i]."<br> ";
 }
 
-echo mb_substr($result, 0, -1);
- 
-var_dump($new_animals);
 ?>
+<html>
+    <head>
+    </head>
+    <body>
+        <h3>1. Исходный массив</h3>
+            <pre>
+                <?php var_dump($continents); ?>
+            </pre>
+        <h3>2. Животные из двух слов</h3>
+                <?php echo implode(", ", $two_words_animals); ?>
+        <h3>3. Новые животные</h3>
+                <?php echo $result; ?>
+    </body>
+</html>
