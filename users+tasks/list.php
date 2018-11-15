5d0ffc5cd04f867eb5b42859b338ae08b1f45c8d
JoeFamiliar <joefamwork@gmail.com>
2018-11-15 22:45:23 +0300

    1

---
+++ users+tasks/list.php
@@ -0,0 +1,20 @@
+<?php
+$path = 'tests/';
+$files = scandir($path);
+$text = '';
+array_shift($files); // удаляем из массива '.'
+array_shift($files); // удаляем из массива '..'
+for($i=0; $i<sizeof($files); $i++){
+	$format = array_pop(explode(".",$files[$i]));             
+	if( $format == 'json'){
+		$text .= '<a href="test.php?test='.$i.'" title="открыть/скачать файл">Тест '.$i.'</a>;<br>';  //записываем все тесты для вывода списка
+	}
+}
+?>
+<html>
+	<head>
+	</head>
+	<body>
+		<?php echo $text; ?>
+	</body>
+</html>
---
+++ users+tasks/test.php
@@ -0,0 +1,51 @@
+<?php
+$path = 'tests/';
+$files = scandir($path);
+array_shift($files); // удаляем из массива '.'
+array_shift($files); // удаляем из массива '..'
+$json = file_get_contents("tests/".$files[$_GET['test']]);
+$test = json_decode($json, true);
+
+$testText = '';
+for($k = 0; $k < count($test); $k++) {
+	$testText .= '<fieldset>';
+	$testText .= '<legend>'.$test[$k]['Question'].'</legend>';
+	for($i = 0; $i < count($test[$k]['variants']); $i++) {
+		$testText .= '<label><input type="radio" name="q'.$k.'" value="'.$test[$k]['variants'][$i].'">'.$test[$k]['variants'][$i].'</label>';
+	}
+	$testText .= '</fieldset>';
+
+}
+
+$result = '';
+$score = 0;
+if(!empty($_POST)) {
+	for($k = 0; $k < count($test); $k++) {
+		$try = $_POST['q'.$k];
+		if($try == '') {
+			$result .= 'Вы не ответили на '.$k.' вопрос. Вы ответили <strong>'.$_POST['q'.$k].'</strong><br>';
+		}
+		elseif ($try == $test[$k]['answer']) {
+			$score ++;
+			$result .= 'Вы правильно ответили на '.$k.' вопрос. Вы ответили <strong>'.$_POST['q'.$k].'</strong><br>';
+		}
+		else {
+			$result .= 'Вы неправильно ответили на '.$k.' вопрос. Вы ответили <strong>'.$_POST['q'.$k].'</strong><br>';
+		}
+	}
+	$result .= 'Количество правильных ответов: <strong>'.$score.'</strong><br/><a href="list.php">Назад к списку тестов</a></p>';
+}						
+?>
+
+<html>
+	<head>
+	</head>
+	<body>
+		<form action="" method="POST">
+ 		<?php echo $testText; ?>
+  			<input type="submit" value="Отправить">  
+		</form>
+		<br>
+		<?php echo $result; ?>
+	</body>
+</html>
