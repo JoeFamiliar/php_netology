e2e60a0374ac3ea194d9ad4f8542815d726f15b3
JoeFamiliar <joefamwork@gmail.com>
2018-11-17 22:41:21 +0300

    Update visa.php

--- json. csv/visa.php
+++ json. csv/visa.php
@@ -1,26 +1,42 @@
-<form method="post">
-	Введите название страны: 
-	<input type="text" name="country" value=""><br>
-	<input type="submit" value="Проверить">
-</form>
 <?php
 $filename = "https://raw.githubusercontent.com/netology-code/php-2-homeworks/master/files/countries/opendata.csv";
 $contents = file_get_contents($filename);
 $lines = explode("\n", $contents);
 $data = [];
-	foreach( $lines as $key => $line ){
-		$data[] = str_getcsv( $line, ","); // linedata
-		unset( $lines[$key] );
-	}
-$country = $_POST['country'];
-$res = 0;
-for($i = 1; $i < count($data); $i++){
-	if($country == $data[$i][1]){
-		echo $data[$i][4];
-		$res = 1;
-	}
+
+foreach( $lines as $key => $line )
+{
+	$data[] = str_getcsv( $line, ","); // linedata
+	unset( $lines[$key] );
 }
-if(!$res){
-	echo "такой страны нет в нашем списке";
+
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+	$country = $_POST['country'];
+	$res = 0;
+	$text = '';
+	for($i = 1; $i < count($data); $i++)
+	{
+		if($country == $data[$i][1]){
+			$text = $data[$i][4];
+			$res = 1;
+		}
+	}
+	if(!$res)
+	{
+		$text = "такой страны нет в нашем списке";
+	}
 }
 ?>
+<html>
+	<head>
+	</head>
+	<body>
+		<form method="post">
+			Введите название страны: 
+			<input type="text" name="country" value=""><br>
+			<input type="submit" value="Проверить">
+		</form>
+		<?php echo $text; ?>
+	</body>
+</html>
