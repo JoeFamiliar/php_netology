3f09cf623eb37644651382cc77eb50bce347419d
JoeFamiliar <joefamwork@gmail.com>
2018-11-23 01:12:05 +0300

    Update admin.php

--- sessions/admin.php
+++ sessions/admin.php
@@ -8,12 +8,20 @@ if(isset($_SESSION['username'])) { // если юзер, то всё ок
 	    if(preg_match("/\.(json)$/", $filename)){
 	    	$tmp_name = $_FILES['json']['tmp_name'];
 	    	move_uploaded_file($tmp_name, "$uploads_dir/$filename");
-	    	$text = '<p>Ваш тест загружен успешно</p>';
+	    	$_SESSION['loadMessage'] = '<p>Ваш тест загружен успешно</p>';
 	    } else {
-	    	$text = '<p>Ошибка! Неверный формат файла теста</p>';
+	    	$_SESSION['loadMessage'] = '<p>Ошибка! Неверный формат файла теста</p>';
 	    }
-	    header('Location: list.php?text='.$text);
+	    header('Location: list.php');
 	}
+	$files = glob($uploads_dir . '*.json');
+	for($i=0; $i<sizeof($files); $i++){
+		$text .= '<p><a href="test.php?test='.$i.'" title="пройти тест">Тест '.$i.'</a>&nbsp;&nbsp;<a href="admin.php?delete='.$files[$i].'">Удалить тест</a></p>';  //записываем все тесты для вывода списка
+	}
+	if (!empty($_GET['delete'])){
+        unlink($_GET['delete']);
+        header('Location: admin.php');
+    }
 } else  { //если не юзер, а гость или вообще никто - кыш
 	header('HTTP/1.0 403 Unauthorized');
 	exit;
@@ -24,7 +32,7 @@ if(isset($_SESSION['username'])) { // если юзер, то всё ок
 	<head>
 	</head>
 	<body>
-		<?php //echo $text; ?>
+		<?php echo $text; ?>
 		<form action="admin.php" method="POST" enctype="multipart/form-data">
 			Загрузить тест:
 			<input type="file" name="json"><br>
