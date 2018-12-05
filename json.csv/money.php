157587fc215328a01ddb66c633786b3235519348
JoeFamiliar <joefamwork@gmail.com>
2018-12-05 14:57:50 +0300

    upd

--- json.csv/money.php
+++ json.csv/money.php
@@ -7,9 +7,7 @@ if (!empty($argv[1])) {
 		$data = fgetcsv($handle, 1000, ",");
 	}
 
-	list(, $sum, $target) = $argv;
-
-	if($sum === "--today") {
+	if($argv[1] === "--today") {
 		$total = 0;
 		// считать данные из csv, пройти в цикле и сравнить текущую дату и вывести только те, у которых даты совпадают
 		while($data !== FALSE){
@@ -20,14 +18,17 @@ if (!empty($argv[1])) {
 			$text = "Общие расходы за сегодня $total р.";
 		}
 	} else {
-
-	$today = date("Y-m-d");
-	$row = [$sum, $target, $today];
-	// записать в csv
-	fputcsv($handle, $row);
-	  
-	$text = "Мы потратили $sum р. на $target $today";
+		list(, $sum, $target) = $argv;
+		$today = date("Y-m-d");
+		$row = [$sum, $target, $today];
+		// записать в csv
+		fputcsv($handle, $row);
+		  
+		$text = "Мы потратили $sum р. на $target $today";
 	}
+
+	echo $text;
+	
 } else {
     exit('Ошибка! Аргументы не заданы. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}');
 }
