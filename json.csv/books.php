66e6e69aefb6856be0fd0c0ff60eb7df798f04db
JoeFamiliar <joefamwork@gmail.com>
2018-12-05 14:52:35 +0300

    upd

--- json.csv/books.php
+++ json.csv/books.php
@@ -1,58 +1,56 @@
 <?php
 
-$filename = "https://www.googleapis.com/books/v1/volumes?q=";
-$handle = fopen("books.csv", "w");
+if (!empty($argv[1])) {
+    
+    $filename = "https://www.googleapis.com/books/v1/volumes?q=";
+    $handle = fopen("books.csv", "w");
 
-if ($handle !== FALSE) 
-{
-	$data = fgetcsv($handle, 1000, ",");
-}
+    if ($handle !== FALSE) 
+    {
+        $data = fgetcsv($handle, 1000, ",");
+    }
 
-$search = $argv;
-if ($argc > 2) {
-	unset($search[0]);
-	$book = implode("%20", $search);
+    $search = $argv;
+
+    if ($argc > 2) {
+    	unset($search[0]);
+    	$book = implode("%20", $search);
+    } else {
+    	$book = $search[1];
+    }
+
+    $contents = file_get_contents($filename.$book);
+    $json = json_decode($contents, true);
+    switch (json_last_error()) {
+        case JSON_ERROR_NONE:
+            echo ' - No errors';
+        break;
+        case JSON_ERROR_DEPTH:
+            echo ' - Maximum stack depth exceeded';
+        break;
+        case JSON_ERROR_STATE_MISMATCH:
+            echo ' - Underflow or the modes mismatch';
+        break;
+        case JSON_ERROR_CTRL_CHAR:
+            echo ' - Unexpected control character found';
+        break;
+        case JSON_ERROR_SYNTAX:
+            echo ' - Syntax error, malformed JSON';
+        break;
+        case JSON_ERROR_UTF8:
+            echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
+        break;
+        default:
+            echo ' - Unknown error';
+        break;
+    }
+
+    foreach ($json['items'] as $searchRes) {
+    	$row = [$searchRes['id'], $searchRes['volumeInfo']['title'], implode(', ', $searchRes['volumeInfo']['authors'])];
+    	fputcsv($handle, $row);
+    }
+
 } else {
-	$book = $search[1];
+    exit('Название книги не было передано');
 }
-$contents = file_get_contents($filename.$book);
-$json = json_decode($contens, true);
-switch (json_last_error()) {
-    case JSON_ERROR_NONE:
-        echo ' - No errors';
-    break;
-    case JSON_ERROR_DEPTH:
-        echo ' - Maximum stack depth exceeded';
-    break;
-    case JSON_ERROR_STATE_MISMATCH:
-        echo ' - Underflow or the modes mismatch';
-    break;
-    case JSON_ERROR_CTRL_CHAR:
-        echo ' - Unexpected control character found';
-    break;
-    case JSON_ERROR_SYNTAX:
-        echo ' - Syntax error, malformed JSON';
-    break;
-    case JSON_ERROR_UTF8:
-        echo ' - Malformed UTF-8 characters, possibly incorrectly encoded';
-    break;
-    default:
-        echo ' - Unknown error';
-    break;
-}
-
-foreach ($json['items'] as $searchRes) {
-	$row = [$searchRes['id'], $searchRes['volumeInfo']['title'], implode(', ', $searchRes['volumeInfo']['authors'])];
-	fputcsv($handle, $row);
-}
-
 ?>
-
-<html>
-	<head>
-    	<meta content="text/html; charset=utf-8">
-	</head>
-	<body>
-		<?php echo $text; ?>
-	</body>
-</html>
--- json.csv/money.php
+++ json.csv/money.php
@@ -1,40 +1,34 @@
 <?php
+if (!empty($argv[1])) {
 
-$handle = fopen("money.csv", "w");
+	$handle = fopen("money.csv", "w");
 
-if($handle !== FALSE){
-	$data = fgetcsv($handle, 1000, ",");
-}
+	if($handle !== FALSE){
+		$data = fgetcsv($handle, 1000, ",");
+	}
 
-list(, $sum, $target) = $argv;
+	list(, $sum, $target) = $argv;
 
-if($sum === "--today") {
-	$total = 0;
-	// считать данные из csv, пройти в цикле и сравнить текущую дату и вывести только те, у которых даты совпадают
-	while($data ! == FALSE){
-		$num = count($data);
-		for($i = 0; $i < $num; $i++){
-			$total += $data[$i]['sum'];
+	if($sum === "--today") {
+		$total = 0;
+		// считать данные из csv, пройти в цикле и сравнить текущую дату и вывести только те, у которых даты совпадают
+		while($data !== FALSE){
+			$num = count($data);
+			for($i = 0; $i < $num; $i++){
+				$total += $data[$i]['sum'];
+			}
+			$text = "Общие расходы за сегодня $total р.";
 		}
-		$text = "Общие расходы за сегодня $total р.";
+	} else {
+
+	$today = date("Y-m-d");
+	$row = [$sum, $target, $today];
+	// записать в csv
+	fputcsv($handle, $row);
+	  
+	$text = "Мы потратили $sum р. на $target $today";
 	}
 } else {
-
-$today = date("Y-m-d");
-$row = [$sum, $target, $today];
-// записать в csv
-fputcsv($handle, $row);
-  
-$text = "Мы потратили $sum р. на $target $today";
+    exit('Ошибка! Аргументы не заданы. Укажите флаг --today или запустите скрипт с аргументами {цена} и {описание покупки}');
 }
-
 ?>
-
-<html>
-	<head>
-    	<meta content="text/html; charset=utf-8">
-	</head>
-	<body>
-		<?php echo $text; ?>
-	</body>
-</html>
